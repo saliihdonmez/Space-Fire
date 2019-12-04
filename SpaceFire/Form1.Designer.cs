414a9c9dca0fddc24a910f6237053ee977eeca4e
Salih Dönmez <saliihdonmez@gmail.com>
2019-12-04 09:39:34 +0300

    ilk

---
+++ SpaceFire/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace SpaceFire
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pBGemi = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.pBCan1 = new System.Windows.Forms.PictureBox();
+            this.pBCan2 = new System.Windows.Forms.PictureBox();
+            this.lblSkor = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pBGemi)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBCan1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBCan2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Controls.Add(this.pBGemi);
+            this.panel1.Location = new System.Drawing.Point(1, 74);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1424, 601);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.Panel1_Paint);
+            // 
+            // pBGemi
+            // 
+            this.pBGemi.Image = ((System.Drawing.Image)(resources.GetObject("pBGemi.Image")));
+            this.pBGemi.Location = new System.Drawing.Point(15, 208);
+            this.pBGemi.Margin = new System.Windows.Forms.Padding(4);
+            this.pBGemi.Name = "pBGemi";
+            this.pBGemi.Size = new System.Drawing.Size(133, 62);
+            this.pBGemi.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBGemi.TabIndex = 0;
+            this.pBGemi.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(1124, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Gidilen Mesafe : 0";
+            // 
+            // pBCan1
+            // 
+            this.pBCan1.Image = ((System.Drawing.Image)(resources.GetObject("pBCan1.Image")));
+            this.pBCan1.Location = new System.Drawing.Point(45, 35);
+            this.pBCan1.Name = "pBCan1";
+            this.pBCan1.Size = new System.Drawing.Size(25, 24);
+            this.pBCan1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBCan1.TabIndex = 2;
+            this.pBCan1.TabStop = false;
+            // 
+            // pBCan2
+            // 
+            this.pBCan2.Image = ((System.Drawing.Image)(resources.GetObject("pBCan2.Image")));
+            this.pBCan2.Location = new System.Drawing.Point(86, 35);
+            this.pBCan2.Name = "pBCan2";
+            this.pBCan2.Size = new System.Drawing.Size(25, 24);
+            this.pBCan2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBCan2.TabIndex = 3;
+            this.pBCan2.TabStop = false;
+            // 
+            // lblSkor
+            // 
+            this.lblSkor.AutoSize = true;
+            this.lblSkor.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSkor.Location = new System.Drawing.Point(797, 35);
+            this.lblSkor.Name = "lblSkor";
+            this.lblSkor.Size = new System.Drawing.Size(75, 24);
+            this.lblSkor.TabIndex = 4;
+            this.lblSkor.Text = "Skor: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1424, 671);
+            this.Controls.Add(this.lblSkor);
+            this.Controls.Add(this.pBCan2);
+            this.Controls.Add(this.pBCan1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Space Fire";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pBGemi)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBCan1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBCan2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pBGemi;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pBCan1;
+        private System.Windows.Forms.PictureBox pBCan2;
+        private System.Windows.Forms.Label lblSkor;
+    }
+}
+
---
+++ SpaceFire/Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SpaceFire
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+        int mesafe = 20;
+        int sayac = 0;
+        double gidilenMesafe = 0;
+
+        int kalanCan = 2;
+        string[] yildizlar;
+        int skor = 0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            YildizYap();
+            timer1.Start();
+            dusman_olustur();
+        }
+
+        void YildizYap()
+        {
+            StreamReader oku = new StreamReader(@"dat//yildiz.txt");
+            string read = oku.ReadToEnd();
+            string[] nesneler = read.Split('/');
+            yildizlar = nesneler;
+
+        }
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                Yukari_Git();
+            }
+
+            else if (e.KeyCode == Keys.Down)
+            {
+                Asagi_Git();
+            }
+
+            if (e.KeyCode == Keys.Space)
+            {
+                mermi_olustur();
+            }
+        }
+
+        void Yukari_Git()
+        {
+            if (pBGemi.Location.Y - mesafe > 0)
+            {
+                pBGemi.Location = new Point(pBGemi.Location.X, pBGemi.Location.Y - mesafe);
+            }
+        }
+
+        void Asagi_Git()
+        {
+            if (pBGemi.Location.Y + mesafe + pBGemi.Height < panel1.Height)
+            {
+                pBGemi.Location = new Point(pBGemi.Location.X, pBGemi.Location.Y + mesafe);
+            }
+
+        }
+
+        void dusman_olustur()
+        {
+            PictureBox enemy = new PictureBox();
+            enemy.Width = 133;
+            enemy.Height = 62;
+            Random rnd = new Random();
+            int x = rnd.Next(0, panel1.Height - enemy.Height);
+
+            enemy.ImageLocation = "enemy.png";
+            enemy.SizeMode = PictureBoxSizeMode.StretchImage;
+            enemy.Location = new Point(panel1.Width - enemy.Width, x);
+            enemy.Tag = "dusman";
+            panel1.Controls.Add(enemy);
+        }
+        void mermi_olustur()
+        {
+            PictureBox mermi = new PictureBox();
+            mermi.Width = 30;
+            mermi.Height = 10;
+            mermi.Location = new Point(pBGemi.Location.X + pBGemi.Width, pBGemi.Location.Y + pBGemi.Height / 2);
+            mermi.BackColor = Color.Red;
+            mermi.Tag = "mermi";
+            panel1.Controls.Add(mermi);
+
+
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            gidilenMesafe += 0.1;
+            sayac++;
+            label1.Text = $"Gidilen Mesafe : {gidilenMesafe.ToString().Split(',')[0]} m";
+     
+
+
+            for (int i = 0; i < panel1.Controls.Count; i++)
+            {
+                PictureBox pb = (PictureBox)panel1.Controls[i];
+                if (panel1.Controls[i].Tag == "mermi")
+                {
+                    if (pb.Location.X + pb.Width > panel1.Width)
+                    {
+                        panel1.Controls.RemoveAt(i);
+                    }
+
+                    else
+                    {
+                        pb.Location = new Point(pb.Location.X + 5, pb.Location.Y);
+                    }
+
+                    for (int a = 0; a < panel1.Controls.Count; a++)
+                    {
+                        if (panel1.Controls[a].Tag == "dusman")
+                        {
+                            if (pb.Location.X + pb.Width > panel1.Controls[a].Location.X && pb.Location.X + pb.Width < panel1.Controls[a].Location.X + panel1.Controls[a].Width)
+                            {
+                                if (pb.Location.Y + pb.Height > panel1.Controls[a].Location.Y && pb.Location.Y + pb.Height < panel1.Controls[a].Location.Y + panel1.Controls[a].Height)
+                                {
+                                    panel1.Controls.RemoveAt(a);
+                                    panel1.Controls.Remove(pb);
+                                    skor += 10;
+                                    lblSkor.Text = $"Skor : {skor}";
+                                 
+
+                                }
+                            }
+                        }
+                    }
+
+                }
+
+                else if (panel1.Controls[i].Tag == "dusman")
+                {
+                    if (pb.Location.X < 0)
+                    {
+                        panel1.Controls.RemoveAt(i);
+                    }
+
+                    else
+                    {
+                        pb.Location = new Point(pb.Location.X - 6, pb.Location.Y);
+                        if (pb.Location.X > pBGemi.Location.X && pb.Location.X < pBGemi.Location.X + pBGemi.Width)
+                        {
+                            if (pb.Location.Y >= pBGemi.Location.Y && pb.Location.Y < pBGemi.Location.Y + pBGemi.Height || pBGemi.Location.Y >= pb.Location.Y && pBGemi.Location.Y < pb.Location.Y + pb.Height)
+                            {
+                                panel1.Controls.RemoveAt(i);
+                                this.Controls.Remove(pBCan2);
+                                kalanCan--;
+                                if (kalanCan == 0)
+                                {
+
+                                    this.Controls.Remove(pBCan1);
+                                    timer1.Stop();
+                                    DialogResult cevap = MessageBox.Show($" Gidilen mesafe: {gidilenMesafe.ToString().Split(',')[0]} m \n Skorunuz: {skor}\n Yeniden Oynamak İster Misiniz? ", "Oyun Bitti", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+
+                                    if (cevap==DialogResult.Yes)
+                                    {
+                                        Application.Restart();
+                                    }
+                                    else
+                                    {
+                                        Environment.Exit(0);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+
+            if (sayac == 60)
+            {
+                sayac = 0;
+
+                dusman_olustur();
+            }
+
+            for (int i = 0; i < yildizlar.Length; i++)
+            {
+                int x = Convert.ToInt32(yildizlar[i].Split(',')[0]);
+                int y = Convert.ToInt32(yildizlar[i].Split(',')[1]);
+
+                if (x < 0)
+                {
+                    x = panel1.Width;
+
+                }
+
+                else
+                {
+                    x--;
+                }
+
+                yildizlar[i] = x.ToString() + "," + y.ToString();
+
+            }
+            panel1.Refresh();
+        }
+
+        private void Panel1_Paint(object sender, PaintEventArgs e)
+        {
+            for (int i = 0; i < yildizlar.Length; i++)
+            {
+                int x = Convert.ToInt32(yildizlar[i].Split(',')[0]);
+                int y = Convert.ToInt32(yildizlar[i].Split(',')[1]);
+                SolidBrush firca = new SolidBrush(Color.White);
+                e.Graphics.FillEllipse(firca, new Rectangle(x, y, 5, 5));
+            }
+        }
+
+ 
+    }
+}
